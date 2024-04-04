e4f9238a3462845de14a318f0aa43556302687af
unknown <nastika.nastik@gmail.com>
2024-04-04 09:41:08 +0300

    apiService.test edit

--- src/tests/apiService.test.ts
+++ src/tests/apiService.test.ts
@@ -8,14 +8,21 @@ describe('postToNest', () => {
   });
 
   it('posts data and returns the response', async () => {
-    const mockData = { id: 1, name: 'Test User' };
+    const mockData = {
+      id: 1,
+      title: "I am in love with someone.",
+      userId: 5
+    };
     fetchMock.mockResponseOnce(JSON.stringify(mockData));
 
-    const formData = { name: 'Test User' };
+    const formData = {  
+      title: 'I am in love with someone.',
+      userId: 5, 
+    };
     const response = await postToNest(formData);
 
     expect(fetch).toHaveBeenCalledTimes(1);
-    expect(fetch).toHaveBeenCalledWith('http://localhost:8082/users', {
+    expect(fetch).toHaveBeenCalledWith('https://dummyjson.com/products/1', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(formData),
@@ -26,7 +33,7 @@ describe('postToNest', () => {
   it('throws an error when the fetch fails', async () => {
     fetchMock.mockRejectOnce(new Error('Fetch failed'));
   
-    const formData = { name: 'Test User' };
+    const formData = { userId: 5};
     let error;
     await postToNest(formData).catch((error) => {
         expect(error).toBeDefined();
